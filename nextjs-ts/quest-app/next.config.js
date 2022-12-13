6c58a6cfaac42c16c36d5cf20edefd5f83fd55ba
Luciano Paci <lucianopaci93@gmail.com>
2022-12-13 10:41:29 -0300

    Added CRA and NextJS templates

---
+++ nextjs-ts/quest-app/next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = {
+  ...nextConfig,
+  webpack(config) {
+    config.module.rules.push({
+      test: /.svg$/i,
+      issuer: /.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config
+  },
+};
